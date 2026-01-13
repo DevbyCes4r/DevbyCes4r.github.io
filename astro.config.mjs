91259eeee3b6ef50edb1af79eab732867daf4e50
ces4r_nm <alejoo2310@gmail.com>
2026-01-13 15:42:20 -0500

    feat: update site URLs to use custom domain devbyces4r.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devbyces4r.github.io',
+  site: 'https://devbyces4r.me',
   output: 'static',
   integrations: [
     mdx({
